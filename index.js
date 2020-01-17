636cb9735ed254b8f0be18da9fa6898023415dbe
Stoneface-Vimes <cliffweir92@gmail.com>
2020-01-16 20:52:20 -0500

    Created two scripts that together use an ip hardcoded to lighthouse labs Toronto, and returns the next 5 ISS passover times and derations. I hit the timebox on part 4 unfortunately

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
+const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log(("It didn't work!", error));
+  }
+  printPassTimes(passTimes);
+  
+});
+
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:', ip);
+
+// });
+
+
+// fetchCoordsByIP('66.207.199.230', (error, geo) => {
+
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+
+//   }
+//   console.log('It worked! returned coords: ', geo);
+// });
+
+// fetchISSFlyOverTimes({ latitude: 43.6383, longitude: -79.4301 }, (error, times) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+
+//   }
+//   console.log('It worked! Returned flyover times: ', times);
+// });
